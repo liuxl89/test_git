379d7e7637b555f6c031e24b5ac1789e1924ad9f
liuxl89 <liuxl89@gmail.com>
2013-09-03 15:08:57 -0400

    initial setup

---
+++ t.C
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+
+#define BIG (1000* 1000* 1000)
+#define pb push_back 
+#define ll long long 
+using namespace std;
+
+
+
+int main()
+{
+   return 0;
+}
